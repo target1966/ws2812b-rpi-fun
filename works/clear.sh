e49681abe9c587c0affdebd745b0fb184ef38948
Dave Wiesmore <target1966@gmail.com>
2025-04-29 14:36:46 -0400

    better

--- works/clear.sh
+++ works/clear.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
 
-sudo works/clear.py
-
+if [ -f works/clear.py ]; then
+  CLEAR=works/clear.py
+elif [ -f ./clear.py ]; then
+  CLEAR=./clear.py
+fi
+if [ -s "$CLEAR" ]; then
+  sudo $CLEAR
+else
+  echo "clear.py not fount"
+fi
